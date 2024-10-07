917c8c9f89bd4e2f0cdb57018ee88e25069d2033
SmiJa <SmiJa13@gmail.com>
2024-10-06 23:58:28 -0700

    build test for leaf node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
   def test_eq(self):
@@ -14,8 +14,8 @@ class TestHTMLNode(unittest.TestCase):
     self.assertEqual(node, node2)
 
   def test_eq_list(self):
-    child1 = HTMLNode("li", "Movies")
-    child2 = HTMLNode("li", "Video Games")
+    child1 = LeafNode("li", "Movies")
+    child2 = LeafNode("li", "Video Games")
     children = [child1, child2]
     node = HTMLNode("ul", None, children, None)
     node2 = HTMLNode("ul", None, children, None)
@@ -25,6 +25,11 @@ class TestHTMLNode(unittest.TestCase):
     node = HTMLNode()
     node2 = HTMLNode()
     self.assertEqual(node, node2)
+
+  def test_eq_leaf(self):
+    node1 = LeafNode("p", "Hello World!").to_html()
+    node2 = "<p>Hello World!</p>"
+    self.assertEqual(node1, node2)
 
 if __name__ == "__main__":
   unittest.main()
